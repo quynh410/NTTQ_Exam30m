1da6cf821f8a23aa8dc35d1715279c05f7242f2a
quynh410 <quynhhhhhh07@gmail.com>
2025-03-31 16:17:00 +0700

    add

--- src/ra/presentation/Main.java
+++ src/ra/presentation/Main.java
@@ -1,39 +1,66 @@
-package ra.presentation;
+package presentation;
 
 import ra.util.Validator;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Nhập số nguyên: ");
-        String intInp =  sc.nextLine();
-        System.out.println(Validator.validInteger(intInp)? "Hợp lê" : "Không hợp lệ");
+        System.out.print("Nhập một số nguyên: ");
+        String intInput = scanner.nextLine();
+        if (Validator.validInteger(intInput)) {
+            System.out.println("Số nguyên hợp lệ");
+        } else {
+            System.out.println("Số nguyên không hợp lệ");
+        }
 
-        System.out.println("Nhập số thưc float: ");
-        String inpFloat = sc.nextLine();
-        System.out.println(Validator.validFloat((inpFloat)) ? "Hợp lê" : "Không hợp lệ");
+        System.out.print("Nhập một số thực (float): ");
+        String floatInput = scanner.nextLine();
+        if (Validator.validFloat(floatInput)) {
+            System.out.println("Số thực hợp lệ");
+        } else {
+            System.out.println("Số thực không hợp lệ");
+        }
 
-        System.out.println("Nhập số Double: ");
-        String inpDouble = sc.nextLine();
-        System.out.println(Validator.validDouble(inpDouble) ? "Hợp lê" : "Không hợp lệ");
+        System.out.print("Nhập một số thực (double): ");
+        String doubleInput = scanner.nextLine();
+        if (Validator.validDouble(doubleInput)) {
+            System.out.println("Số thực hợp lệ");
+        } else {
+            System.out.println("Số thực không hợp lệ");
+        }
 
-        System.out.println("Nhập giá trị boolean true/false: ");
-        String inpBoolean = sc.nextLine();
-        System.out.println(Validator.validBoolean(inpBoolean)? "Hợp lê" : "Không hợp lệ");
+        System.out.print("Nhập một giá trị boolean (true/false): ");
+        String boolInput = scanner.nextLine();
+        if (Validator.validBoolean(boolInput)) {
+            System.out.println("Giá trị boolean hợp lệ");
+        } else {
+            System.out.println("Giá trị boolean không hợp lệ");
+        }
 
-        System.out.println("Nhập chuỗi 1 chuỗi : ");
-        String inp1 = sc.nextLine();
-        System.out.println(Validator.validString(inp1 ,1,20) ? "Hợp lê" : "Không hợp lệ");
+        System.out.print("Nhập một chuỗi: ");
+        String strInput = scanner.nextLine();
+        if (Validator.validString(strInput, 3, 10)) {
+            System.out.println("Chuỗi hợp lệ");
+        } else {
+            System.out.println("Chuỗi không hợp lệ");
+        }
 
-        System.out.println("Nhập email: ");
-        String inpEmail = sc.nextLine();
-        System.out.println(Validator.validEmail(inpEmail)? "Hợp lê" : "Không hợp lệ");
+        System.out.print("Nhập một địa chỉ email: ");
+        String emailInput = scanner.nextLine();
+        if (Validator.validEmail(emailInput)) {
+            System.out.println("Email hợp lệ");
+        } else {
+            System.out.println("Email không hợp lệ");
+        }
 
-        System.out.println("Nhập số điện thoại : ");
-        String inpPhone = sc.nextLine();
-        System.out.println(Validator.validPhone(inpPhone)? "Hợp lê" : "Không hợp lệ");
+        System.out.print("Nhập một số điện thoại di động Việt Nam: ");
+        String phoneInput = scanner.nextLine();
+        if (Validator.validPhone(phoneInput)) {
+            System.out.println("Số điện thoại hợp lệ");
+        } else {
+            System.out.println("Số điện thoại không hợp lệ");
+        }
     }
 }
